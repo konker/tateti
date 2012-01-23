2fc670d4ff12cede11863e81cf568a8cf6f26c52
konker <konker@gmail.com>
2012-01-23 20:37:01 +0200

    Added basic test for simple game

--- test/unit/tateti.js
+++ test/unit/tateti.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     module("Module tateti.js");
 
-    test("Board", function() {
-        expect(1);
+    test("Board OK", function() {
+        expect(6);
 
         var b = new tateti.Board();
         equals(b.toString(),
@@ -10,7 +10,53 @@ $(document).ready(function() {
                "| .  | .  | .  |\n" +
                "| .  | .  | .  |\n" +
                "| .  | .  | .  |\n" +
-               " --------------"
-         );
+               " --------------",
+        "Empty board");
+
+        b.set(tateti.P1, tateti.A);
+        equals(b.toString(),
+               " --------------\n"  +
+               "| P1 | .  | .  |\n" +
+               "| .  | .  | .  |\n" +
+               "| .  | .  | .  |\n" +
+               " --------------",
+        "Move 1");
+
+        b.set(tateti.P2, tateti.B);
+        equals(b.toString(),
+               " --------------\n"  +
+               "| P1 | P2 | .  |\n" +
+               "| .  | .  | .  |\n" +
+               "| .  | .  | .  |\n" +
+               " --------------",
+        "Move 2");
+
+        b.set(tateti.P1, tateti.D);
+        equals(b.toString(),
+               " --------------\n"  +
+               "| P1 | P2 | .  |\n" +
+               "| P1 | .  | .  |\n" +
+               "| .  | .  | .  |\n" +
+               " --------------",
+        "Move 3");
+
+        b.set(tateti.P2, tateti.E);
+        equals(b.toString(),
+               " --------------\n"  +
+               "| P1 | P2 | .  |\n" +
+               "| P1 | P2 | .  |\n" +
+               "| .  | .  | .  |\n" +
+               " --------------",
+        "Move 4");
+
+        b.set(tateti.P1, tateti.G);
+        equals(b.toString(),
+               " --------------\n"  +
+               "| P1 | P2 | .  |\n" +
+               "| P1 | P2 | .  |\n" +
+               "| P1 | .  | .  |\n" +
+               " --------------",
+        "Move 5");
+
     });
 });
