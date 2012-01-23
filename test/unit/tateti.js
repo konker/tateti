b6b44cde6a8afac3e8189d834ec3f2acc4d0f2ad
konker <konker@gmail.com>
2012-01-23 20:54:51 +0200

    Added some tests for exceptions

--- test/unit/tateti.js
+++ test/unit/tateti.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     module("Module tateti.js");
 
     test("Board OK", function() {
-        expect(6);
+        expect(10);
 
         var b = new tateti.Board();
         equals(b.toString(),
@@ -49,14 +49,62 @@ $(document).ready(function() {
                " --------------",
         "Move 4");
 
-        b.set(tateti.P1, tateti.G);
+        b.set(tateti.P1, tateti.H);
         equals(b.toString(),
                " --------------\n"  +
                "| P1 | P2 | .  |\n" +
                "| P1 | P2 | .  |\n" +
-               "| P1 | .  | .  |\n" +
+               "| .  | P1 | .  |\n" +
                " --------------",
         "Move 5");
 
+        b.set(tateti.P2, tateti.I);
+        equals(b.toString(),
+               " --------------\n"  +
+               "| P1 | P2 | .  |\n" +
+               "| P1 | P2 | .  |\n" +
+               "| .  | P1 | P2 |\n" +
+               " --------------",
+        "Move 6");
+
+        b.move(tateti.H, tateti.G);
+        equals(b.toString(),
+               " --------------\n"  +
+               "| P1 | P2 | .  |\n" +
+               "| P1 | P2 | .  |\n" +
+               "| P1 | .  | P2 |\n" +
+               " --------------",
+        "Move 7");
+
+        ok(b.gameOver, "Game over");
+
+        deepEqual(b.checkWinner(), [tateti.A, tateti.D, tateti.G], "P1 Winner");
     });
+
+    test("Wrong turn", function() {
+        expect(1);
+
+        var b = new tateti.Board();
+
+        b.set(tateti.P1, tateti.A);
+        raises(function() {
+                b.set(tateti.P1, tateti.B);
+            },
+            "Wrong turn detected"
+        );
+    });
+
+    test("Illegal set", function() {
+        expect(1);
+
+        var b = new tateti.Board();
+
+        b.set(tateti.P1, tateti.A);
+        raises(function() {
+                b.set(tateti.P2, tateti.A);
+            },
+            "Illegal set detected"
+        );
+    });
+
 });
