40ac2e6fff854d166c3d700911d1afc076e2aaa4
konker <konker@gmail.com>
2012-01-25 19:15:16 +0200

    Added 'ai' module skeleton code (mistakenly missed in the last commit)

---
+++ js/tateti-ai.js
@@ -0,0 +1,21 @@
+/**
+    tateti-ai.js
+    Ta Te Ti computer player logic
+
+    Depends on tateti.js
+
+    Konrad Markus <konker@gmail.com>
+
+ */
+
+if (typeof(tateti) == 'undefined') {
+    throw "tateti object dependency not found";
+}
+
+
+tateti.ai = {
+    init: function() {
+    }
+}
+$(tateti.ai.init);
+
